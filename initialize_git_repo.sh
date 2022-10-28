6e67d765af1570b621aed186e6f9f170266919f1
Tristan Arjoon <tda35@miami.edu>
2022-10-28 11:13:31 -0400

    wdwdw

---
+++ initialize_git_repo.sh
@@ -0,0 +1,6 @@
+   18  git innit 
+   19  git init
+   20  ls -al
+   21  git checkout -b main
+   22  history | tail -n 6 > initialize_git_repo.shell
+   23  history | tail -n 6 > initialize_git_repo.sh
